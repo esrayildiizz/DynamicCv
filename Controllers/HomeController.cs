e3d4e4f69b4f65b8af168d1f3ccf7402327d65d7
Esra YILDIZ <esrayildiizz@outlook.com>
2022-11-14 15:50:42 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        //OnePages :Tek sayfada tüm sayfaları duzenlendi.
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
